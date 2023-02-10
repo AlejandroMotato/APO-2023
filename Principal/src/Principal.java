1572f101bedc6bd8bc964ddab02f0e63c83efa22
Estudiante USC <estudiante@GTACAD.USC>
2023-02-10 16:31:15 -0500

    segunda

--- Principal/src/Principal.java
+++ Principal/src/Principal.java
@@ -1,25 +1,26 @@
 import java.util.Scanner;
 
-
 // nombre clase
 public class Principal {
-	
-	
-	
 
-
-
-
-	//metodo principal
+	// metodo principal
 	public static void main(String[] args) {
+
+		// para llamar de otro metodo
+		System.out.println(contarPalabras()); 
+	}
+
+	public static String contarPalabras() {
 		System.out.println("ingrese la palabra");
-		
-		//permite capturar datos por consola
+
+		// permite capturar datos por consola
 		Scanner sc = new Scanner(System.in);
-		
+
 		String prueba = sc.next();
-		System.out.println("la cantidad de letras es : " + prueba.toUpperCase());
+		
+		return "la cantidad de letras es : " + prueba.toUpperCase();
 		
 		
+
 	}
 }
