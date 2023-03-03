281ae97051e80edf043854f717479616c0f7f4ce
AlejandroMotato <124595990+AlejandroMotato@users.noreply.github.com>
2023-03-03 12:29:47 -0500

    Add files via upload

---
+++ taller3/src/taller3/Taller.java
@@ -0,0 +1,23 @@
+package taller3;
+
+public class Taller {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+			// TODO Auto-generated method stub
+
+
+			
+			String string1 = "Hola";
+			String string2 = "Mundo";
+
+			
+			String string3 = string1 + string2;
+
+			
+			System.out.println(string3); 
+
+		}
+	}
+
