ec7e316d5029c678c6558fbbf3c4fbf31aae269c
Arpra4586 <supreme_4586@hotmail.com>
2023-05-05 16:47:22 -0500

    taller11

---
+++ Taller11/src/soluciob/Factorial.java
@@ -0,0 +1,14 @@
+package soluciob;
+
+
+public class Factorial extends Punto4 {
+    public Factorial(int num) {
+        super(num);
+    }
+
+    public static void main(String[] args) {
+        Factorial factorialDemo = new Factorial(10);
+        System.out.println(factorialDemo);
+    }
+}
+
